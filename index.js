1d55545878d2ec899683aff892f01d013eac240b
Kimi <mrgaonju@gmail.com>
2019-07-12 15:56:49 +0800

    optimize dispatch and getState function

--- index.js
+++ index.js
@@ -2,13 +2,10 @@
 function redux(reducer, initialState, stream) {
   let state = initialState;
   let action;
-  const dispatch = function (action) {
+  const dispatch = action => {
     state = reducer(state, action)
-    return state
   }
-  const getState = function () {
-    return state
-  }
+  const getState = () => state;
   for(let i = 0; i < stream.length; i++) {
     let action = stream[i];
     dispatch(action)
