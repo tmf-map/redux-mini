ce86ac73f6a7a9ddbf3e9235fb2b450902756e7f
Kimi Gao <mrgaonju@gmail.com>
2019-07-14 13:33:16 +0800

    export dispatch and stream event list (#5)
    * export dispatch and stream event list
    
    * use real event call for demo
    
    * fix console log tips
    
    * make data visiable in html

---
+++ demo/demo.js
@@ -0,0 +1,44 @@
+import { createStore } from '../index.js'
+
+//====================================
+// How to use
+//====================================
+
+// reducer :: a -> b -> a
+function reducer(state, action) {
+  switch (action.type) {
+    case 'SET_NAME':
+      return {
+        ...state,
+        name: action.payload.name
+      }
+    case 'SET_AGE':
+      return {
+        ...state,
+        age: action.payload.age
+      }
+  }
+}
+
+const initialState = {name: 'Kimi', age: 18};
+const store = createStore(reducer, initialState)
+
+const dataElem = document.getElementById('data')
+function render(state) {
+  console.log('state', state)
+  dataElem.innerHTML = `State: ${JSON.stringify(state)}`
+}
+
+render(store.getState()) // {name: 'Kimi', age: 18}
+
+// event stream call
+window.setName = function () {
+  const action = {type: 'SET_NAME', payload: {name: 'Robbie'}}
+  store.dispatch(action)
+  render(store.getState())
+}
+window.setAge = function () {
+  const action = {type: 'SET_AGE', payload: {age: 16}}
+  store.dispatch(action)
+  render(store.getState())
+}
