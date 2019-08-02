fdcd9d8699c3cef02c4ee375dbea4c5bf6e5e9c9
USTC-Han <839057430@qq.com>
2019-08-02 17:26:40 +0800

    add combineReducer function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // createStoreWithoutEnhancers :: (a -> b -> a) -> a -> c
-function createStoreWithoutEnhancers (reducer, initialState) {
-  let state = initialState;
+function createStoreWithoutEnhancers (reducer) {
+  let state = {name:{name: 'Kimi'}, age:{age: 18}} ;
   return {
     dispatch: function(action) {
       state = reducer(state, action)
@@ -23,12 +23,42 @@ function applyMiddleware (middlewares) {
 }
 
 // createStore :: (a -> b -> a) -> a -> ([d] -> c) -> c
-function createStore (reducer, initialState, enhancers) {
-  const store = createStoreWithoutEnhancers(reducer, initialState)
+function createStore (reducer, enhancers) {
+  const store = createStoreWithoutEnhancers(reducer)
   return enhancers(store)
+}
+
+function combineReducer(reducers) {
+  const finalReducers = {};
+
+  for (let key in reducers) {
+    // 遍历传入的reducer，如果是函数类型放入finalReducers
+    if(reducers[key] instanceof Function) {
+      finalReducers[key] = reducers[key];
+    }
+  }
+
+  return function (state = {}, action) {
+    let stateChange = false;
+    let nextState = {};
+
+    for (let key in finalReducers) {
+      // 取reducer和对应的state
+      const reducer = finalReducers[key];
+      const preStateItem = state[key];
+
+      // 拿到action后更新子state，将子state保存在新的nextState中
+      const nextStateItem = reducer(preStateItem, action);
+      nextState[key] = nextStateItem;
+
+      stateChange = stateChange || nextStateItem !== preStateItem
+    }
+    return stateChange ? nextState : state;
+  }
 }
 
 export {
   createStore,
-  applyMiddleware
+  applyMiddleware,
+  combineReducer
 }
